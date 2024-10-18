60828058ef92901722f50da58353e77ce3691059
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-10-18 15:14:09 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Hello world \n";
+    $variavel = 10;
+    $variavel2 = 20;
+    if($variavel1 < $variavel2){
+        echo $variavel + $variavel2 . "\n";
+    } echo $variavel - $variavel2;
